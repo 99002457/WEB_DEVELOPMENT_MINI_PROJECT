c852ff1ee9991876b62d882f274bfaf76fb275f1
99002457 <chethan.gc@ltts.com>
2020-11-13 12:52:53 +0530

    Added all files

--- 3_Implementation/src/server.js
+++ 3_Implementation/src/server.js
@@ -44,6 +44,31 @@ io.on('connection', socket => {
     });
   });
 
+  // Listen for chatMessage
+  socket.on('chatMessage', msg => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+
+      // Send users and room info
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      });
+    }
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
